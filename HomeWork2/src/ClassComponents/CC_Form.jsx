ba306042a6e5689aef46d5b30aef1678130ef508
AviaPerelman <154759994+AviaPerelman@users.noreply.github.com>
2024-01-30 13:32:10 +0200

    matla2 uplaod

---
+++ HomeWork2/src/ClassComponents/CC_Form.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Input_text from "../Functional Components/FC_InputText";
+
+export default class CCForm extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+            lastNameMessage: "",
+            firstNameMessage: "",
+            psychometricScoreMessage: "",
+            schoolMessage: ""
+
+        }
+
+    }
+
+    ShowMessage = (e) => {
+
+        let textInput = e.target.id;
+
+        let newMessage = `You must fill in a ${textInput}.`;
+
+        this.setState({ [`${textInput}Message`]: newMessage })
+
+    }
+
+
+    HideMessage = (e) => {
+
+        let textInput = e.target.id;
+
+        this.setState({ [`${textInput}Message`]: "" });
+        
+        const inputValue = e.target.value;
+
+        if (textInput == "psychometricScore" && inputValue != "") {
+
+            if (inputValue < 555) {
+                this.setState({ [`schoolMessage`]: "Try again next year!" });
+            }
+            else {
+                this.setState({ [`schoolMessage`]: "You can be accepted for studies" });
+            }
+
+        }
+
+    }
+
+    render() {
+
+        // I used functional component to create the div element for the input text because the code repats it self
+
+        return (
+            <>
+                <form>
+                    <Input_text message={this.state.lastNameMessage} type="text" label="Last name: " id="lastName" onFocus={this.ShowMessage} onBlur={this.HideMessage} />
+                    <Input_text message={this.state.firstNameMessage} type="text" label="First name: " id="firstName" onFocus={this.ShowMessage} onBlur={this.HideMessage} />
+                    <Input_text message={this.state.psychometricScoreMessage} type="number" label="Psychometric score: " id="psychometricScore" onFocus={this.ShowMessage} onBlur={this.HideMessage} />
+                    <p> {this.state.schoolMessage}</p>
+                </form>
+            </>
+
+        );
+    }
+} 
